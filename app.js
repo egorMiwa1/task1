923d67b58807f02704585449b7b42305255fa8cf
egorMiwa1 <egorMiwa@yandex.ru>
2021-12-20 03:26:44 +0300

    small fix

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ export default function appScr(
       console.log(URL);
       const browser = await puppeteer.launch({
         headless: true,
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        args: ["--no-sandbox"],
       });
       const page = await browser.newPage();
       await page.goto(URL);
